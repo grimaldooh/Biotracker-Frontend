c096a5dad3b515fcda57667f4b29b53b243686d3
grimaldooh <angel.grimaldo@uabc.edu.mx>
2025-08-16 22:05:49 -0700

    .

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,34 +1,37 @@
 import { ApplicationConfig, importProvidersFrom, isDevMode, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withInterceptors, HTTP_INTERCEPTORS } from '@angular/common/http';
-//import { provideAnimations } from '@angular/platform-browser/animations';
-
-import { routes } from './app.routes';
+import { provideHttpClient, withInterceptorsFromDi, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { provideServiceWorker } from '@angular/service-worker';
 
-// Importa los módulos de Angular Material
+import { routes } from './app.routes';
+
+// Material
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatButtonModule } from '@angular/material/button';
+
 import { AuthInterceptor } from './core/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    // Temporalmente deshabilitar service worker para debug
     provideServiceWorker('ngsw-worker.js', {
-      enabled: !isDevMode(),
+      enabled: false,
       registrationStrategy: 'registerWhenStable:30000'
     }),
-    provideHttpClient(),
-    //provideAnimations(),// para que sirve? sirve 
+
+    provideHttpClient(withInterceptorsFromDi()),
+
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptor,
       multi: true
     },
+
     importProvidersFrom(
       MatFormFieldModule,
       MatInputModule,
